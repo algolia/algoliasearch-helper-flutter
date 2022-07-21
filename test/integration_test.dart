d39243fe40bafcd99f859b7c32068df50a2a0c02
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-07-21 16:44:37 +0200

    update tests

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -2,22 +2,20 @@ import 'package:algolia_helper/algolia_helper.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Operation callback', () async {
+  test('Successful search operation', () async {
     final helper = AlgoliaHelper.create(
       applicationID: 'latency',
       apiKey: 'afc3dd66dd1293e2e2736a5a51b05c0a',
       indexName: 'instant_search',
-      state: const SearchState(hitsPerPage: 1),
+      state: const SearchState(query: "apple", hitsPerPage: 1),
     );
 
-    helper.query("apple");
     var response = await helper.responses.take(1).first;
-
     expect(response.hits.length, 1);
     expect(response.query, "apple");
   });
 
-  test('Operation failed', () async {
+  test('Failing search operation', () async {
     final helper = AlgoliaHelper.create(
         applicationID: 'latency',
         apiKey: 'UNKNOWN',
