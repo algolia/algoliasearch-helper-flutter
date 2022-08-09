e67d53f1d99100401e6497d11a6d412d67756ab2
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-08-09 12:00:59 +0200

    refactor: rename libraries

--- helper_flutter/lib/algolia.dart
+++ helper_flutter/lib/algolia.dart
@@ -1,4 +1,8 @@
-library helper_flutter;
+/// Algolia search helpers for Flutter
+///
+/// Provides patterns and APIs that make it possible to customize search
+/// experiences at a deeper level for Flutter framework.
+library algolia_helper_flutter;
 
 export 'package:algolia_helper_dart/algolia.dart'; // expose dart helpers
 
