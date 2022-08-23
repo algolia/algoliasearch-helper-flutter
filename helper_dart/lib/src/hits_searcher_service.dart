bc9672638bdc4b52cb84f2d80bdd0a7911990c8c
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-08-23 11:12:00 +0200

    update hits service query build

--- helper_dart/lib/src/hits_searcher_service.dart
+++ helper_dart/lib/src/hits_searcher_service.dart
@@ -67,89 +67,34 @@ extension AlgoliaExt on Algolia {
   /// Create [AlgoliaQuery] instance based on [state].
   AlgoliaQuery queryOf(SearchState state) {
     AlgoliaQuery query = index(state.indexName);
-    state.query?.let((it) => query = query.query(it));
-    state.page?.let((it) => query = query.setPage(it));
-    state.hitsPerPage?.let((it) => query = query.setHitsPerPage(it));
-    state.query?.let((it) => query = query.query(it));
-    state.facets?.let((it) => query = query.setFacets(it));
-    state.ruleContexts?.let((it) => query = query.setRuleContexts(it));
-
-    state.query?.let((it) => query = query.query(it));
-    state.page?.let((it) => query = query.setPage(it));
-    state.hitsPerPage?.let((it) => query = query.setHitsPerPage(it));
-    state.facets?.let((it) => query = query.setFacets(it));
-    state.filterGroups?.let((it) => query = query.setFilterGroups(it));
-    state.ruleContexts?.let((it) => query = query.setRuleContexts(it));
-    state.facetFilters?.let((it) => query = query.setFacetFilters(it));
-    state.advancedSyntax
-        ?.let((it) => query = query.setAdvancedSyntax(enabled: it));
-    state.allowTyposOnNumericTokens
-        ?.let((it) => query = query.setAllowTyposOnNumericTokens(it));
     state.analytics?.let((it) => query = query.setAnalytics(enabled: it));
-    state.analyticsTags?.let((it) => query = query.setAnalyticsTags(it));
-    state.aroundLatLng?.let((it) => query = query.setAroundLatLng(it));
-    state.aroundLatLngViaIP
-        ?.let((it) => query = query.setAroundLatLngViaIP(it));
-    state.aroundPrecision?.let((it) => query = query.setAroundPrecision(it));
-    state.aroundRadius?.let((it) => query = query.setAroundRadius(it));
     state.attributesToHighlight
         ?.let((it) => query = query.setAttributesToHighlight(it));
     state.attributesToRetrieve
         ?.let((it) => query = query.setAttributesToRetrieve(it));
     state.attributesToSnippet
         ?.let((it) => query = query.setAttributesToSnippet(it));
-    state.clickAnalytics
-        ?.let((it) => query = query.setClickAnalytics(enabled: it));
-    state.disableExactOnAttributes
-        ?.let((it) => query = query.setDisableExactOnAttributes(it));
-    state.disableTypoToleranceOnAttributes
-        ?.let((it) => query = query.setDisableTypoToleranceOnAttributes(it));
-    state.distinct?.let((it) => query = query.setDistinct(value: it));
-    state.enableABTest?.let((it) => query = query.setEnableABTest(enabled: it));
-    state.enablePersonalization
-        ?.let((it) => query = query.setEnablePersonalization(enabled: it));
-    state.enableRules?.let((it) => query = query.setEnableRules(enabled: it));
-    state.facetingAfterDistinct
-        ?.let((it) => query = query.setFacetingAfterDistinct(enable: it));
-    state.getRankingInfo
-        ?.let((it) => query = query.setGetRankingInfo(enabled: it));
+    state.facetFilters?.let((it) => query = query.setFacetFilters(it));
+    state.facets?.let((it) => query = query.setFacets(it));
+    state.facets?.let((it) => query = query.setFacets(it));
     state.highlightPostTag?.let((it) => query = query.setHighlightPostTag(it));
     state.highlightPreTag?.let((it) => query = query.setHighlightPreTag(it));
-    state.ignorePlurals?.let((it) => query = query.setIgnorePlurals(it));
-    state.length?.let((it) => query = query.setLength(it));
+    state.hitsPerPage?.let((it) => query = query.setHitsPerPage(it));
     state.maxFacetHits?.let((it) => query = query.setMaxFacetHits(it));
     state.maxValuesPerFacet
         ?.let((it) => query = query.setMaxValuesPerFacet(it));
-    state.minWordSizeFor1Typo
-        ?.let((it) => query = query.setMinWordSizeFor1Typo(it));
-    state.minWordSizeFor2Typos
-        ?.let((it) => query = query.setMinWordSizeFor2Typos(it));
-    state.minimumAroundRadius
-        ?.let((it) => query = query.setMinimumAroundRadius(it));
-    state.naturalLanguages?.let((it) => query = query.setNaturalLanguages(it));
     state.numericFilters?.let((it) => query = query.setNumericFilters(it));
-    state.offset?.let((it) => query = query.setOffset(it));
     state.optionalFilters?.let((it) => query = query.setOptionalFilters(it));
-    state.percentileComputation
-        ?.let((it) => query = query.setPercentileComputation(enabled: it));
-    state.personalizationImpact
-        ?.let((it) => query = query.setPersonalizationImpact(value: it));
-    state.queryLanguages?.let((it) => query = query.setQueryLanguages(it));
-    state.removeStopWords?.let((it) => query = query.setRemoveStopWords(it));
-    state.replaceSynonymsInHighlight
-        ?.let((it) => query = query.setReplaceSynonymsInHighlight(enabled: it));
-    state.restrictHighlightAndSnippetArrays?.let(
-        (it) => query = query.setRestrictHighlightAndSnippetArrays(enable: it));
-    state.restrictSearchableAttributes
-        ?.let((it) => query = query.setRestrictSearchableAttributes(it));
-    state.similarQuery?.let((it) => query = query.setSimilarQuery(it));
-    state.snippetEllipsisText
-        ?.let((it) => query = query.setSnippetEllipsisText(it));
+    state.page?.let((it) => query = query.setPage(it));
+    state.page?.let((it) => query = query.setPage(it));
+    state.query?.let((it) => query = query.query(it));
+    state.query?.let((it) => query = query.query(it));
+    state.query?.let((it) => query = query.query(it));
+    state.ruleContexts?.let((it) => query = query.setRuleContexts(it));
+    state.ruleContexts?.let((it) => query = query.setRuleContexts(it));
     state.sumOrFiltersScore
         ?.let((it) => query = query.setSumOrFiltersScore(it));
-    state.synonyms?.let((it) => query = query.setSynonyms(enabled: it));
     state.tagFilters?.let((it) => query = query.setTagFilters(it));
-    state.typoTolerance?.let((it) => query = query.setTypoTolerance(it));
     state.userToken?.let((it) => query = query.setUserToken(it));
     return query;
   }
@@ -162,8 +107,9 @@ extension AlgoliaExt on Algolia {
         );
 }
 
+/// Extensions over [AlgoliaQuery].
 extension AlgoliaQueryExt on AlgoliaQuery {
-  /// Filter hits by facet value
+  /// Filter hits by facet value.
   AlgoliaQuery setFacetFilters(List<String> facetFilters) {
     var query = this;
     for (var facetList in facetFilters) {
@@ -172,6 +118,7 @@ extension AlgoliaQueryExt on AlgoliaQuery {
     return query;
   }
 
+  /// Filter on numeric attributes.
   AlgoliaQuery setNumericFilters(List<String> numericFilters) {
     var query = this;
     for (var numericFilter in numericFilters) {
@@ -180,10 +127,21 @@ extension AlgoliaQueryExt on AlgoliaQuery {
     return query;
   }
 
+  /// Create filters for ranking purposes, where records that match the filter
+  /// are ranked highest.
   AlgoliaQuery setOptionalFilters(List<String> optionalFilters) {
     var query = this;
     for (var optionalFilter in optionalFilters) {
       query = query.setOptionalFilter(optionalFilter);
+    }
+    return query;
+  }
+
+  /// Filter hits by tags.
+  AlgoliaQuery setTagFilters(List<String> tagFilters) {
+    var query = this;
+    for (var tagFilter in tagFilters) {
+      query = query.setTagFilter(tagFilter);
     }
     return query;
   }
