62d1b451ad379e85a67f0e17fcbe88cc9148ab17
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-08-25 17:35:13 +0200

    generate single TextSpan with children TextSpans instead of the list, improve test

--- helper_flutter/lib/src/highlighting.dart
+++ helper_flutter/lib/src/highlighting.dart
@@ -3,18 +3,21 @@ import 'package:flutter/widgets.dart';
 
 extension HighlightingExt on HighlightedString {
   /// Converts [HighlightedString] to [TextSpan] list.
-  List<TextSpan> toTextSpans([
+  TextSpan toTextSpan([
     TextStyle regularTextStyle = const TextStyle(fontWeight: FontWeight.normal),
     TextStyle highlightedTextStyle =
         const TextStyle(fontWeight: FontWeight.bold),
   ]) =>
-      tokens
-          .map(
-            (token) => TextSpan(
-              text: token.content,
-              style:
-                  token.highlighted ? highlightedTextStyle : regularTextStyle,
-            ),
-          )
-          .toList();
+      TextSpan(
+        children: tokens
+            .map(
+              (token) => TextSpan(
+                text: token.content,
+                style: token.isHighlighted
+                    ? highlightedTextStyle
+                    : regularTextStyle,
+              ),
+            )
+            .toList(),
+      );
 }
--- helper_flutter/test/helper_flutter_test.dart
+++ helper_flutter/test/helper_flutter_test.dart
@@ -1,11 +1,40 @@
 import 'package:algolia_helper_flutter/algolia.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
     const string = 'This <em>John</em> Doe looks like <em>John</em>athan.';
     final highlighted = HighlightedString.of(string);
-    final spans = highlighted.toTextSpans();
-    expect(spans.length, 5);
+    const regularStyle = TextStyle(fontWeight: FontWeight.normal, color: Color(0xFF421133));
+    const highlightedStyle = TextStyle(fontWeight: FontWeight.bold, color: Color(0xFF42A5F5));
+    final span = highlighted.toTextSpan(regularStyle, highlightedStyle);
+    expect(span.children?.length, 5);
+    span.children?.asMap().forEach((index, value) {
+      switch (index) {
+        case 0:
+          expect(value.toPlainText(), 'This ');
+          expect(value.style, regularStyle);
+          break;
+        case 1:
+          expect(value.toPlainText(), 'John');
+          expect(value.style, highlightedStyle);
+          break;
+        case 2:
+          expect(value.toPlainText(), ' Doe looks like ');
+          expect(value.style, regularStyle);
+          break;
+        case 3:
+          expect(value.toPlainText(), 'John');
+          expect(value.style, highlightedStyle);
+          break;
+        case 4:
+          expect(value.toPlainText(), 'athan.');
+          expect(value.style, regularStyle);
+          break;
+        default:
+          break;
+      }
+    });
   });
 }
