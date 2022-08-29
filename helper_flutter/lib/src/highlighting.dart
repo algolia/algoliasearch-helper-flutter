30d3309a7c2bfb627e37fe1fd67633f27654de5e
Vladislav Fitc <vladislav.fitz@gmail.com>
2022-08-29 21:39:56 +0200

    apply changes from review

--- helper_flutter/lib/src/highlighting.dart
+++ helper_flutter/lib/src/highlighting.dart
@@ -1,25 +1,53 @@
 import 'package:algolia_helper_dart/algolia.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 extension HighlightingExt on HighlightedString {
   /// Converts [HighlightedString] to [TextSpan].
   /// Applies [regularTextStyle] and [highlightedTextStyle] styles to
   /// to non-highlighted and highlighted sub-spans accordingly
-  TextSpan toTextSpan([
+  TextSpan toTextSpan({
+    TextStyle? style,
     TextStyle regularTextStyle = const TextStyle(fontWeight: FontWeight.normal),
     TextStyle highlightedTextStyle =
         const TextStyle(fontWeight: FontWeight.bold),
-  ]) =>
+    GestureRecognizer? recognizer,
+    MouseCursor? mouseCursor,
+    PointerEnterEventListener? onEnter,
+    PointerExitEventListener? onExit,
+    String? semanticsLabel,
+    Locale? locale,
+    bool? spellOut,
+  }) =>
       TextSpan(
-        children: tokens
-            .map(
-              (token) => TextSpan(
-                text: token.content,
-                style: token.isHighlighted
-                    ? highlightedTextStyle
-                    : regularTextStyle,
-              ),
-            )
-            .toList(),
-      );
+          style: style,
+          children: toInlineSpans(
+              regularTextStyle: regularTextStyle,
+              highlightedTextStyle: highlightedTextStyle,),
+          recognizer: recognizer,
+          mouseCursor: mouseCursor,
+          onEnter: onEnter,
+          onExit: onExit,
+          semanticsLabel: semanticsLabel,
+          locale: locale,
+          spellOut: spellOut,);
+
+  /// Converts [HighlightedString] to list of [InlineSpan].
+  /// Applies [regularTextStyle] and [highlightedTextStyle] styles to
+  /// to non-highlighted and highlighted sub-spans accordingly
+  List<InlineSpan> toInlineSpans({
+    TextStyle regularTextStyle = const TextStyle(fontWeight: FontWeight.normal),
+    TextStyle highlightedTextStyle =
+        const TextStyle(fontWeight: FontWeight.bold),
+  }) =>
+      tokens
+          .map(
+            (token) => TextSpan(
+              text: token.content,
+              style:
+                  token.isHighlighted ? highlightedTextStyle : regularTextStyle,
+            ),
+          )
+          .toList();
 }
