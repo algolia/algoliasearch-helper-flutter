f4df0fee4c83cd83df5784037ae28bb6124aa60d
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-08-31 19:14:27 +0200

    apply lint changes

--- helper_dart/lib/src/immutable_filters.dart
+++ helper_dart/lib/src/immutable_filters.dart
@@ -139,20 +139,6 @@ class ImmutableFilters extends Filters {
         numericGroups: numericGroups ?? this.numericGroups,
         hierarchicalGroups: hierarchicalGroups ?? this.hierarchicalGroups,
       );
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      super == other &&
-          other is ImmutableFilters &&
-          runtimeType == other.runtimeType &&
-          facetGroups.equals(other.facetGroups) &&
-          tagGroups.equals(other.tagGroups) &&
-          numericGroups.equals(other.numericGroups) &&
-          hierarchicalGroups.equals(other.hierarchicalGroups);
-
-  @override
-  int get hashCode => super.hashCode;
 }
 
 /// Extensions over [FilterGroupMap].
