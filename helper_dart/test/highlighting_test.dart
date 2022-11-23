8a86dc0d93de0488016f3c53230809166adfbe13
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-11-23 17:52:03 +0100

    fix(highlighting): strings with symbols other then letters (#46)

--- helper_dart/test/highlighting_test.dart
+++ helper_dart/test/highlighting_test.dart
@@ -5,7 +5,12 @@ void main() {
   test('Highlight tokens generation', () {
     const string = 'This <em>John</em> Doe looks like <em>John</em>athan.';
     final highlighted = HighlightedString.of(string);
-    print(highlighted);
     expect(highlighted.tokens.length, 5);
+  });
+
+  test('Highlight tokens generation with apostrophe', () {
+    const string = "<em>John's</em> looks like <em>Jones</em>";
+    final highlighted = HighlightedString.of(string);
+    expect(highlighted.tokens.length, 3);
   });
 }
