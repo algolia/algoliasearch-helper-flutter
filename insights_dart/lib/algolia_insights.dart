3a879f20ac983b43ca1cc1b5471ec05558bf4001
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-04-04 11:21:49 +0200

    make event class public, add code documentation

--- insights_dart/lib/algolia_insights.dart
+++ insights_dart/lib/algolia_insights.dart
@@ -1,8 +1,9 @@
 /// Algolia Insights Helper
 library algolia_helper_flutter;
 
-export 'src/event_tracker.dart';
+export 'src/event.dart';
 export 'src/event_data_delegate.dart';
+export 'src/event_tracker.dart';
 export 'src/filter_event_tracker.dart';
 export 'src/filter_event_tracker.dart';
 export 'src/hits_event_tracker.dart';
--- insights_dart/lib/src/event.dart
+++ insights_dart/lib/src/event.dart
@@ -1,26 +1,41 @@
 enum EventType { click, conversion, view }
 
+/// The `Event` class represents an Algolia Insights event, which is used
+/// for collecting user interaction data in order to provide analytics and
+/// improve search relevance.
 class Event {
+  /// The type of the event (click, conversion, or view).
   EventType type;
 
+  /// The name of the event.
   String eventName;
 
+  /// The name of the Algolia index associated with the event.
   String indexName;
 
+  /// The unique identifier for the user who triggered the event.
   String userToken;
 
+  /// The timestamp when the event occurred (optional).
   DateTime? timestamp;
 
+  /// The query ID associated with the event (optional).
   String? queryID;
 
+  /// The list of object IDs associated with the event (optional).
   Iterable<String>? objectIDs;
 
+  /// The positions of clicked or converted items (optional).
   Iterable<int>? positions;
 
+  /// The attribute associated with filter-based events (optional).
   String? attribute;
 
+  /// The list of filter values associated with filter-based events (optional).
   Iterable<String>? filterValues;
 
+  /// A private constructor to create an Event object with the provided
+  /// parameters.
   Event._(
     this.type,
     this.eventName,
@@ -34,6 +49,7 @@ class Event {
     this.filterValues,
   });
 
+  /// Creates a click event after a search with object IDs and positions.
   Event.clickHitsAfterSearch(
     String eventName,
     String indexName,
@@ -53,6 +69,7 @@ class Event {
           objectIDs: objectIDs,
         );
 
+  /// Creates a click event with object IDs.
   Event.clickHits(
     String eventName,
     String indexName,
@@ -68,6 +85,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a conversion event after a search with object IDs.
   Event.convertHitsAfterSearch(
     String eventName,
     String indexName,
@@ -85,6 +103,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a conversion event with object IDs.
   Event.convertHits(
     String eventName,
     String indexName,
@@ -100,6 +119,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a view event with object IDs.
   Event.viewHits(
     String eventName,
     String indexName,
@@ -115,6 +135,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a click event with filter attribute and values.
   Event.clickFilters(
     String eventName,
     String indexName,
@@ -132,6 +153,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a view event with filter attribute and values.
   Event.viewFilters(
     String eventName,
     String indexName,
@@ -149,6 +171,7 @@ class Event {
           timestamp: timestamp,
         );
 
+  /// Creates a conversion event with filter attribute and values.
   Event.convertFilters(
     String eventName,
     String indexName,
