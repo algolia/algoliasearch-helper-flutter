d2dfcc40876cfe8310b44c49a7894c96ba55fd49
Vladislav Fitc <vladislav.fitc@algolia.com>
2023-07-17 22:31:45 +0200

    fix: use app documents directory to store the user token if possible (#88)

--- insights_dart/lib/src/user_token_storage.dart
+++ insights_dart/lib/src/user_token_storage.dart
@@ -1,5 +1,9 @@
 import 'package:hive/hive.dart';
+import 'package:logging/logging.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
+
+import 'platform_web.dart' if (dart.library.io) 'platform_native.dart';
 
 class UserTokenStorage {
   static const _userTokenKey = 'insights-user-token';
@@ -11,6 +15,9 @@ class UserTokenStorage {
 
   /// A pseudonymous or anonymous user identifier.
   String get userToken => _userToken;
+
+  /// Events logger
+  final Logger _log = Logger('Algolia/UserTokenStorage');
 
   set userToken(String userToken) {
     _userToken = userToken;
@@ -34,11 +41,23 @@ class UserTokenStorage {
     }
   }
 
-  String _boxPath;
-  String _boxName = 'userToken';
+  String? _boxPath;
+  String _boxName = 'user-token';
 
   /// Box value persistently storing the user token and its lease time
-  Future<Box> get _box => Hive.openBox(_boxName, path: _boxPath);
+  Future<Box> get _box async {
+    final String path;
+    if (_boxPath != null) {
+      path = _boxPath!;
+    } else if (isWeb) {
+      path = 'algolia';
+    } else {
+      final docsDir = await getApplicationDocumentsDirectory();
+      path = '${docsDir.path}/algolia';
+    }
+    _log.fine('Open box with path $path');
+    return Hive.openBox(_boxName, path: path);
+  }
 
   /// Private value storing the actual value of the persistent storage allowance
   /// flag
@@ -59,7 +78,7 @@ class UserTokenStorage {
 
   /// Value storing the unique UserTokenStorage instance
   static final UserTokenStorage _sharedInstance =
-      UserTokenStorage.custom('algolia', 'user-token');
+      UserTokenStorage.custom(null, 'algolia-user-token');
 
   /// Factory constructor returning the unique UserTokenStorage instance
   factory UserTokenStorage() => _sharedInstance;
@@ -68,7 +87,10 @@ class UserTokenStorage {
   UserTokenStorage.custom(this._boxPath, this._boxName) {
     read().then((storedUserToken) {
       if (storedUserToken != null) {
+        _log.fine('Successfully read non-expired user token: $storedUserToken');
         userToken = storedUserToken;
+      } else if (allowPersistentUserTokenStorage) {
+        _write(userToken);
       }
     });
   }
@@ -77,6 +99,8 @@ class UserTokenStorage {
   void _write(String userToken) {
     final expirationDate =
         DateTime.now().millisecondsSinceEpoch + leaseTime * 60 * 1000;
+    _log.fine('Write to persistent storage user token $userToken, '
+        'expiration date $expirationDate');
     _box.then(
       (box) => box
         ..put(_userTokenKey, userToken)
@@ -86,6 +110,7 @@ class UserTokenStorage {
 
   /// Remove user token and its expiration date from persistent storage
   void remove() {
+    _log.fine('Clear persistent storage');
     _box.then((box) {
       if (box.isOpen) {
         box
@@ -103,6 +128,8 @@ class UserTokenStorage {
     final storedUserTokenExpirationDate = await box.get(
       _expirationDateKey,
     ) as int?;
+    _log.fine('Read from persistent storage user token: $storedUserToken, '
+        'expiration date: $storedUserTokenExpirationDate');
     if (storedUserToken != null &&
         storedUserTokenExpirationDate != null &&
         DateTime.now().millisecondsSinceEpoch < storedUserTokenExpirationDate) {
