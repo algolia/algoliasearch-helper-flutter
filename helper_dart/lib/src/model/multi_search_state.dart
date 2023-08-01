7fd702efcffcc8c2a0aec9fa0dcf01eec997d15c
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-08-01 17:19:58 +0200

    remove overrides from multisearchstate

--- helper_dart/lib/src/model/multi_search_state.dart
+++ helper_dart/lib/src/model/multi_search_state.dart
@@ -4,11 +4,5 @@ part 'search_state.dart';
 part 'facet_search_state.dart';
 
 sealed class MultiSearchState {
-  @override
-  bool operator ==(Object other);
-
-  @override
-  int get hashCode;
-
   const MultiSearchState();
 }
