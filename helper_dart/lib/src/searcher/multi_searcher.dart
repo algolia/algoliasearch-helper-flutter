2e609a59de37633bbcb1c0a0e81ed2efa44043df
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-08-01 17:53:40 +0200

    add documentation for multi_searcher

--- helper_dart/lib/src/searcher/multi_searcher.dart
+++ helper_dart/lib/src/searcher/multi_searcher.dart
@@ -15,41 +15,118 @@ import '../service/proxy_hits_search_service.dart';
 import 'facet_searcher.dart';
 import 'hits_searcher.dart';
 
+/// Core component for multi-search requests and managing search sessions in
+/// Algolia Helpers.
+///
+/// The [MultiSearcher] enables you to search for hits and facet values in
+/// different indices of the same Algolia application simultaneously. You can
+/// add multiple [HitsSearcher] and [FacetSearcher] instances to the
+/// [MultiSearcher], each targeting different indices or facets,
+///
+/// ## Create MultiSearcher
+///
+/// Instantiate [MultiSearcher] using the constructor:
+///
+/// ```dart
+/// final multiSearcher = MultiSearcher(
+///   _service,
+///   _eventTracker,
+/// );
+/// ```
+///
+/// Or, use the `algolia` named constructor to create a [MultiSearcher] with
+/// Algolia as the search service:
+///
+/// ```dart
+/// final multiSearcher = MultiSearcher.algolia(
+///   'MY_APPLICATION_ID',
+///   'MY_API_KEY',
+///   eventTracker: Insights('MY_APPLICATION_ID', 'MY_API_KEY'),
+/// );
+/// ```
+///
+/// ## Add Searchers
+///
+/// You can add [HitsSearcher] or [FacetSearcher] instances to the
+/// [MultiSearcher] using the provided methods:
+///
+/// - For [HitsSearcher], use `addHitsSearcher`:
+///
+/// ```dart
+/// final hitsSearcher = multiSearcher.addHitsSearcher(
+///   initialState: SearchState(indexName: 'MY_INDEX_NAME'),
+/// );
+/// ```
+///
+/// - For [FacetSearcher], use `addFacetSearcher`:
+///
+/// ```dart
+/// final facetSearcher = multiSearcher.addFacetSearcher(
+///   state: SearchState(indexName: 'MY_INDEX_NAME'),
+///   facet: 'category',
+///   facetQuery: 'shoes',
+/// );
+/// ```
+///
+/// ## Dispose
+///
+/// Call [dispose] to release underlying resources:
+///
+/// ```dart
+/// multiSearcher.dispose();
+/// ```
 class MultiSearcher implements Disposable {
   final MultiSearchService _service;
   final EventTracker _eventTracker;
-  List<MultiSearchStateProvider> stateProviders;
-  final List<MultiSearchResponseReceiver> _services;
+  final List<MultiSearchStateProvider> _stateProviders;
+  final List<MultiSearchResponseReceiver> _responseReceivers;
   StreamSubscription<List<MultiSearchResponse>>? _resultsSubscription;
   bool _isDisposed = false;
 
+  /// Creates a new instance of [MultiSearcher].
+  ///
+  /// - `service`: The [MultiSearchService] to handle multi-search operations.
+  /// - `eventTracker`: The [EventTracker] to track events during search
+  /// operations.
   MultiSearcher(this._service, this._eventTracker)
-      : stateProviders = [],
-        _services = [];
+      : _stateProviders = [],
+        _responseReceivers = [];
 
+  /// Creates a new instance of [MultiSearcher] using Algolia as the search
+  /// service.
+  ///
+  /// - `applicationID`: The Algolia Application ID for your Algolia account.
+  /// - `apiKey`: The Algolia API Key associated with your Algolia account.
+  /// - `eventTracker`: (optional) The [EventTracker] to track events during
+  /// search operations. If not provided, an [Insights] instance will be used by
+  /// default.
   MultiSearcher.algolia(
     String applicationID,
     String apiKey, {
     EventTracker? eventTracker,
   })  : _service = AlgoliaMultiSearchService(applicationID, apiKey),
         _eventTracker = eventTracker ?? Insights(applicationID, apiKey),
-        stateProviders = [],
-        _services = [];
+        _stateProviders = [],
+        _responseReceivers = [];
 
   /// Adds a new [HitsSearcher] to the multi-searcher.
+  ///
+  /// Returns the created [HitsSearcher] instance.
   HitsSearcher addHitsSearcher({
     required SearchState initialState,
     bool disjunctiveFacetingEnabled = true,
   }) {
     final service = ProxyHitsSearchService();
     final searcher = HitsSearcher.custom(service, _eventTracker, initialState);
-    stateProviders.add(searcher);
-    _services.add(service);
+    _stateProviders.add(searcher);
+    _responseReceivers.add(service);
     _updateSubscriptions();
     return searcher;
   }
 
   /// Adds a new [FacetSearcher] to the multi-searcher.
+  ///
+  /// Returns the created [FacetSearcher] instance.
   FacetSearcher addFacetSearcher({
     required SearchState state,
     required String facet,
@@ -64,8 +141,8 @@ class MultiSearcher implements Disposable {
         facetQuery: facetQuery,
       ),
     );
-    stateProviders.add(searcher);
-    _services.add(service);
+    _stateProviders.add(searcher);
+    _responseReceivers.add(service);
     _updateSubscriptions();
     return searcher;
   }
@@ -73,7 +150,7 @@ class MultiSearcher implements Disposable {
   void _updateSubscriptions() {
     _resultsSubscription?.cancel();
     _resultsSubscription = Rx.combineLatest(
-      stateProviders.map((e) => e.multiSearchState),
+      _stateProviders.map((e) => e.multiSearchState),
       (states) => states.cast<MultiSearchState>(),
     )
         .debounceTime(
@@ -82,7 +159,7 @@ class MultiSearcher implements Disposable {
         .asyncMap(_service.search)
         .listen((responses) {
       for (var i = 0; i < responses.length; i++) {
-        _services[i].updateMultiResponse(responses[i]);
+        _responseReceivers[i].updateMultiResponse(responses[i]);
       }
     });
   }
@@ -91,7 +168,7 @@ class MultiSearcher implements Disposable {
   void dispose() {
     _resultsSubscription?.cancel();
     _isDisposed = true;
-    for (var service in _services) {
+    for (var service in _responseReceivers) {
       service.dispose();
     }
   }
