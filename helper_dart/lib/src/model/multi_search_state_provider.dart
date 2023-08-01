5aeef0e44b2ec3f2f2d0312e7d2b3af2839d1764
Vladislav Fitc <vladislav.fitz@gmail.com>
2023-08-01 18:07:50 +0200

    document multi search response receiver and state provider

--- helper_dart/lib/src/model/multi_search_state_provider.dart
+++ helper_dart/lib/src/model/multi_search_state_provider.dart
@@ -1,6 +1,15 @@
 import '../disposable.dart';
+import '../searcher/multi_searcher.dart';
 import 'multi_search_state.dart';
-
+/// A contract for providing multi-search state to [MultiSearcher].
+///
+/// Classes that implement [MultiSearchStateProvider] should provide a [Stream]
+/// of [MultiSearchState] that represents the individual states of multiple
+/// searchers within the [MultiSearcher] component. [MultiSearchState] is an
+/// interface that acts as a common type for [SearchState] and
+/// [FacetSearchState].
 abstract class MultiSearchStateProvider extends Disposable {
+  /// Stream of [MultiSearchState] representing the individual states of
+  /// searchers.
   Stream<MultiSearchState> get multiSearchState;
 }
